3db2a821526c9f10883fe601cd50e46bd98ad46f
Nayan <nayan.bhagowaty@gmail.com>
2022-04-30 11:46:41 +0530

    Update Program.cs

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -3,11 +3,9 @@ using CommandService.Data;
 using CommandService.EventProcessor;
 using CommandService.SyncDataService.Grpc;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 var _env = builder.Environment;
 ConfigurationManager configuration = builder.Configuration;
-
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
@@ -18,18 +16,14 @@ builder.Services.AddScoped<ICommandRepository, CommandRepository>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddHostedService<MessageBusSubscriber>();
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 // app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 PreparationDb.PrePopulationDb(app);
 app.Run();
